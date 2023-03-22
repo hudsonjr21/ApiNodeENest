b6dc1e2bc24adda8d0a978af018442cfa4af21ea
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-03-22 14:57:16 -0300

    criação de tabela no banco de dados - erro

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,22 +8,30 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const dist_1 = require("@nestjs/config/dist");
 const typeorm_1 = require("@nestjs/typeorm");
+const todo_module_1 = require("./app/todo/todo.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
-            typeorm_1.TypeOrmModule.forRoot({
-                type: 'mysql',
-                host: 'localhost',
-                port: 3306,
-                username: 'root',
-                password: '1234',
-                database: 'test',
-                entities: [],
-                synchronize: true,
+            typeorm_1.TypeOrmModule.forRootAsync({
+                imports: [config_1.ConfigModule],
+                inject: [dist_1.ConfigService],
+                useFactory: (configService) => ({
+                    type: 'mysql',
+                    host: configService.get('DB_HOST', 'localhost'),
+                    port: Number(configService.get('DB_PORT', 3306)),
+                    username: configService.get('DB_USERNAME', 'root'),
+                    password: configService.get('DB_PASSWORD', '1234'),
+                    database: configService.get('DB_DATABASE', 'test'),
+                    entities: [__dirname + '/**/*.entity{.js,.ts}'],
+                    synchronize: true,
+                })
             }),
+            todo_module_1.TodoModule,
         ],
         controllers: [],
         providers: [],
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,25 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigService } from '@nestjs/config/dist';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './app/todo/todo.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports:[ConfigModule],
       inject: [ConfigService],
-      useFactory: (ConfigService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.get('DB_HOST','localhost'),
-        port: 3306,
-        username: 'root',
-        password: '1234',
-        database: 'test',
-        entities: [],
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 3306)),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', '1234'),
+        database: configService.get('DB_DATABASE', 'test'),
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
         synchronize: true,
       })
     }),
+    TodoModule,
   ],
   controllers: [],
   providers: [],
